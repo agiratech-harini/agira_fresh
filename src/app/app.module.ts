f2ec3e6a08a410d379dd97c9d482a253c5df3e2a
agiratech-harini <harini.r@agiratech.com>
2024-02-21 18:25:53 +0530

    checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Card1Component } from './card1/card1.component';
+import { ArticleComponent } from './article/article.component';
+import { PodcastComponent } from './podcast/podcast.component';
+import { EBookComponent } from './e-book/e-book.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Card1Component,
+    ArticleComponent,
+    PodcastComponent,
+    EBookComponent,
+   
   ],
   imports: [
     BrowserModule,
